c924a72ec34b6d981de6cf3797dc57aeaef87696
Matthew Sianarta <71192423+Krynvel@users.noreply.github.com>
2024-02-16 10:58:24 -0800

    Update Arduino main.ino

--- Lab 3/Arduino main.ino
+++ Lab 3/Arduino main.ino
@@ -1,4 +1,4 @@
-/ Lab– Joystick
+// Lab– Joystick
 int joyButton = 2; // digital pin # for joy-stick switch:2
 int joyX = A0; // analog pin # for ‘x’ output: A0
 int joyY = A1; // analog pin # for ‘y’ output: A1
@@ -8,11 +8,18 @@ int bluPin = 11; // digital pin # for blue LED; must be PWM!
 float posX = 0; // x-position = float from 0 to 1,023
 float posY = 0; // y-position = float from 0 to 1,023
 int isButtonOn = 0; // analog button state set as Boolean
+float redLevel=0;
+float grnLevel=0;
+float bluLevel=0;
 
 void setup() {
+setRgb(redLevel, grnLevel, bluLevel);
 pinMode(joyButton, INPUT); // joy-stick button is INPUT
 pinMode(joyX, INPUT); // joy-stick x-direction is INPUT
 pinMode(joyY, INPUT); // joy-stick y-direction is INPUT
+pinMode(redPin, OUTPUT);
+pinMode(bluPin, OUTPUT);
+pinMode(grnPin, OUTPUT);
 Serial.begin(9600); // start serial communications
 }
 
@@ -20,6 +27,12 @@ void loop(){
 isButtonOn = digitalRead(joyButton); //read button state& register
 posX = analogRead(joyX); // read x-position
 posY = analogRead(joyY); // read y-position
+redLevel = getRedLevel(posX, posY); // find red level
+grnLevel = getGrnLevel(posX, posY); // find green level
+bluLevel = getBluLevel(posX,posY);
+setRgb(redLevel, grnLevel, bluLevel); //'setRGB' function to update colors
+if(isButtonOn) {
+delay(5000)
 Serial.print("\nSerial"); // display the values on Serial Monitor Window
 Serial.print("Button: ");
 if(isButtonOn) {
@@ -67,11 +80,11 @@ float grnLevel = 0;
 if(posX < 171) {
 grnLevel = map(posX, 0, 170, 0, 255); //grnLevel linearly increases from 0 to 255 in this range
 }
-if(posX>=171 && <512){
+else if(posX>=171 && posX<512){
 grnLevel=255; //greenLevel at max brightness
 }
-else if(posX>=512 && <682){
-  grnLevel=map(posX, 512, 681, 255, 0)
+else if(posX>=512 && posX<682){
+  grnLevel=map(posX, 512, 681, 255, 0);
 }
 else if(posX >= 341 && posX < 682) {
 grnLevel = 0; // redLevel remains at 0 in this range
@@ -89,17 +102,17 @@ float getBluLevel(float posX, float posY){
   if(posX<341){
     bluLevel=0;
     }
-  else if (posX>=341 && <512){
+  else if (posX>=341 && posX<512){
     bluLevel=map(posX,341, 511, 0, 255 );
   }
-  else if(posX>=512 && <653){
-  blueLevel=255;
+  else if(posX>=512 && posX<653){
+  bluLevel=255;
   }
-  else if (posX>=653 && <1023){
-    blueLevel=map(posX,643,1022,255,0);
+  else if (posX>=653 && posX<1023){
+    bluLevel=map(posX,643,1022,255,0);
   }
   else{
     bluLevel=255;
   }
-  returnmap(posY, 1023,0,128,bluLevel);
+  return map(posY, 1023,0,128,bluLevel);
 }
